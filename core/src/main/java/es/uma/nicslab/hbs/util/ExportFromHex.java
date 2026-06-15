550a07a99b5219116bbdf14ad75acf32250d4874
isaamz19 <isabel.munozruiz19@gmail.com>
2026-06-15 13:09:52 +0200

    Actualización del README.md

--- core/src/main/java/es/uma/nicslab/hbs/util/ExportFromHex.java
+++ core/src/main/java/es/uma/nicslab/hbs/util/ExportFromHex.java
@@ -5,6 +5,7 @@ import java.util.HexFormat;
 
 public class ExportFromHex {
     public static void main(String[] args) throws Exception {
+        // Poner aquí la clave pública del bulletin board para obtener el archivo que necesita OpenSSL
         String hex = "000000050000000376c7743c174b4ba46176ef968a38ac1c6db730898bbb8272c66b9a70316711ef32d77dd0eb42e8094521f7de4729aa4a";
 
         byte[] keyBytes = HexFormat.of().parseHex(hex);
